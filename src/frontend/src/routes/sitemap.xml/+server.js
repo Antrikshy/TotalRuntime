c8bd9e3e7cbb822515c5f83b22f121e836451299
Antriksh Yadav <antrikshy@gmail.com>
2024-11-03 20:06:48 -0500

    Fixed sitemap in prod by removing dependence on localhost address

--- src/frontend/src/routes/sitemap.xml/+server.js
+++ src/frontend/src/routes/sitemap.xml/+server.js
@@ -1,9 +1,9 @@
 import axios from "axios"
-import { PUBLIC_BACKEND_REVERSE_PROXY_PORT } from '$env/static/public';
+import { PUBLIC_BACKEND_REVERSE_PROXY_PORT } from "$env/static/public";
 
-export async function GET() {
+export async function GET({ request }) {
   return new Response(
-    (await axios.get(`http://localhost:${PUBLIC_BACKEND_REVERSE_PROXY_PORT}/sitemap`)).data,
-    { headers: { 'Content-Type': 'application/xml' } }
+    (await axios.get(`https://${request.headers.get("host")}:${PUBLIC_BACKEND_REVERSE_PROXY_PORT}/sitemap`)).data,
+    { headers: { "Content-Type": "application/xml" } }
   )
 }
